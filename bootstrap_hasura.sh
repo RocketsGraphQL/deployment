f3b6c380302a3335de5d59b49d21ec5fdaf6f39b
Kaushik Varanasi <kaushik.varanasi1@gmail.com>
2021-09-10 17:53:58 +0530

    sleep while hasura is up and running, then exec curls

---
+++ bootstrap_hasura.sh
@@ -0,0 +1,60 @@
+docker-compose up -d
+
+# Wait for the hasura to be up
+bash -c 'while [[ "$(curl http://localhost:8080/healthz)" != "OK" ]]; do sleep 5; done'
+
+# Connect DB
+curl -d '{
+  "type": "pg_add_source",
+  "args": {
+    "name": "postgres",
+    "configuration": {
+      "connection_info": {
+        "database_url": {
+          "from_env": "PG_DATABASE_URL"
+        },
+        "pool_settings": {
+          "retries": 1,
+          "idle_timeout": 180,
+          "max_connections": 50
+        }
+      }
+    }
+  }
+}
+' -H "Content-Type: application/json" -X POST http://localhost:8080/v1/metadata
+
+# Create table
+curl -d '
+    {
+        "type": "run_sql",
+        "source": "postgres",
+        "args": {
+            "source": "postgres",
+            "cascade": true,
+            "sql": "CREATE TABLE users(id serial NOT NULL, name text NOT NULL, email text NOT NULL, passwordhash text NOT NULL, PRIMARY KEY (id));"
+        }
+    }
+' -H "Content-Type: application/json" -X POST http://localhost:8080/v2/query
+
+
+# Track table
+curl -d '
+    {
+        "type":"bulk",
+        "source":"postgres",
+        "resource_version":2,
+        "args":[
+            {
+                "type":"pg_track_table",
+                "args":{
+                    "table":{
+                        "name":"users",
+                        "schema":"public"
+                    },
+                    "source":"postgres"
+                }
+            }
+        ]
+    }
+' -H "Content-Type: application/json" -X POST http://localhost:8080/v1/metadata
