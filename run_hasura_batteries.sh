6b5320e8f3f4b46a629c3002dd9774fbccaed1d3
Kaushik Varanasi <kaushik.varanasi1@gmail.com>
2021-09-10 19:06:23 +0530

    Link Hasura batteries to Hasura

--- run_hasura_batteries.sh
+++ run_hasura_batteries.sh
@@ -1,2 +1,2 @@
 docker pull rocketsgraphql/hasura-batteries:latest
-docker run -p 7000:7000 -d rocketsgraphql/hasura-batteries
+docker run -p 7000:7000 --name=hasura-batteries -d rocketsgraphql/hasura-batteries
